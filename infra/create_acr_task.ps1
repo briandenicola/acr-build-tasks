993661de4ec3a9e2976017082edd39ae79bd0558
Brian Denicola <bjd145@yahoo.com>
2020-06-09 08:19:18 -0500

    doc updates for github rename

--- infra/create_acr_task.ps1
+++ infra/create_acr_task.ps1
@@ -3,8 +3,8 @@ Writ-Host "Cerating Azure ACR Build Task"
 
 az acr task create -n base-w2k19 -r bjd145 `
     -t windows/bjdbasewindows2019:ltsc2019 `
-    -f ACR/Dockerfile.base                 `
+    -f src/Dockerfile.base                 `
     --platform windows                     `
-    --context https://github.com/bjd145/Kubernetes.git `
+    --context https://github.com/briandenicola/acr-build-tasks.git `
     --commit-trigger-enabled false         `
     --pull-request-trigger-enabled false
